606a3f99cfcde3877ab76e218ff551ea318be7db
seohee0112 <ghdtj7@gmail.com>
2021-12-15 18:04:49 +0900

    [ADD] SplashScreen 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
-import * as React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import { View, Text, Button } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './store/store';
@@ -8,10 +9,15 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import AuthStackScreen from './navigation/AuthStackScreen';
 import MainTabScreen from './navigation/MainTab';
+import { FLOWPREDICATE_TYPES } from '@babel/types';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <NavigationContainer>
